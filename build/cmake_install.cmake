866049ecea61e8dff15ff2d8db1ae30891cd08ac
Jakob <Jakob@DESKTOP-ASC7I92>
2026-06-05 01:05:16 +0200

    updated build

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/jakob/Desktop/ir_remote_gui
+# Install script for directory: C:/Users/Jakob/Desktop/irremote/ir-remote
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,7 +35,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/jakob/Desktop/ir_remote_gui/build/install_local_manifest.txt"
+  file(WRITE "C:/Users/Jakob/Desktop/irremote/ir-remote/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -51,6 +51,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/jakob/Desktop/ir_remote_gui/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/Jakob/Desktop/irremote/ir-remote/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
